598aaecc3b6a3ce99a1111eb9d0b8c9b28c4cd1d
FauzanHadi2 <ozynino@gmail.com>
2022-01-16 16:58:23 +0800

    Initial commit
    Signed-off-by: FauzanHadi2 <ozynino@gmail.com>

---
+++ app/src/main/java/com/example/uas_ojan/model/Itik.java
@@ -0,0 +1,10 @@
+package com.example.uas_ojan.model;
+
+public class Itik extends Hewan{
+
+    public Itik(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Itik",ras,asal,deskripsi,drawableRes);
+
+    }
+
+}
